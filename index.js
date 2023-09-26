d6cad4b3bb909040d03360ebeb1244307bccc49c
Singh, Manjot <ms155@iupui.edu>
2023-09-25 23:44:31 -0400

    Added a few comments into index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// Load environment variables from a .env file located in the same directory as this script
 require("dotenv").config({ path: __dirname + "/.env" });
+
+// Import required modules and libraries
 const { twitterClient } = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
 const express = require('express')
@@ -11,16 +14,20 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-
+// Define the tweet function
 const tweet = async () => {
-
+    // URL of the image to be tweeted
     const uri = "https://picsum.photos/200/300";
     const filename = "image.png";
 
+    // Download the image from the URL
     download(uri, filename, async function(){
         try {
+            // Upload the downloaded image to Twitter and get the media ID
             const mediaId = await twitterClient.v1.uploadMedia("./image.png");
             console.log(mediaId)
+            
+            // Post a tweet with the uploaded media
             await twitterClient.v2.tweet({
                 text: "Hello world! This is an image of something!!",
                 media: {
@@ -33,9 +40,6 @@ const tweet = async () => {
     });
 }
 
-
-const interval = setInterval(tweet, 1800000);
-
-// To stop the interval at some point, you can use:
-// clearInterval(interval);
+// Set up an interval to call the tweet function every 60 seconds
+const interval = setInterval(tweet, 60000); // 60000 milliseconds = 60 seconds
 
